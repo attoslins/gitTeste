e2dcb465c405e653504d93f0d4489ceb1f050188
Attos Lins <attoslins@icloud.com>
2018-02-10 20:21:53 -0200

    adicionado o projeto e os pacotes com a classe escola

---
+++ testeGit/src/br/com/fiap/entity/Escola.java
@@ -0,0 +1,24 @@
+package br.com.fiap.entity;
+
+public class Escola {
+	public String nome;
+
+	public Escola(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Escola() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
